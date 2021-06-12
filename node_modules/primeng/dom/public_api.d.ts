dd8a7d72e3258134fedba46ea9c8b8cf06f3fe03
trangntt-016 <trangntt.016@gmail.com>
2021-06-12 18:29:59 -0400

    remove nodemodule

--- node_modules/primeng/dom/public_api.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './domhandler';
-export * from './connectedoverlayscrollhandler';
--- node_modules/primeng/focustrap/focustrap.d.ts
+++
@@ -1,9 +0,0 @@
-import { ElementRef } from '@angular/core';
-export declare class FocusTrap {
-    el: ElementRef;
-    pFocusTrapDisabled: boolean;
-    constructor(el: ElementRef);
-    onkeydown(e: any): void;
-}
-export declare class FocusTrapModule {
-}
